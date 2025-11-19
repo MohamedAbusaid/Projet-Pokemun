db05b5541a9d30e341979447112e8b6acd13789b
MohamedAbusaid <149703951+MohamedAbusaid@users.noreply.github.com>
2025-11-19 17:07:12 +0100

    Add files via upload

---
+++ DU_BRAZIL/DUBRAZIL/src/main/java/com/mycompany/dubrazil/DUBRAZIL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dubrazil;
+
+/**
+ *
+ * @author mabusaid
+ */
+public class DUBRAZIL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
